1c52db00caf939ed4df7f0be3447bde58d47cd76
Elton Santos <eltonaxl@hotmail.com>
2022-11-29 12:19:46 -0300

    Trying do logout

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,22 @@
+import { FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import * as S from "./styles"
 
 export function Menu() {
+
+  const navigate = useNavigate();
+
+  function logout(event: FormEvent) {
+      event.preventDefault();
+      console.log('Logout');
+
+      // CLEAR DATA FROM STORAGE
+      localStorage.removeItem('@eurorcamento');
+      sessionStorage.removeItem('@eurorcamento');
+
+      navigate("/");
+  }
+
   return (
     <S.Container>
       <ul>
@@ -8,7 +24,7 @@ export function Menu() {
           <button>Alterar Tema</button>
         </li>
         <li>
-          <a href="/">Sair</a>
+          <Link to="#" onClick={logout}>Sair</Link>
         </li>
       </ul>
     </S.Container>

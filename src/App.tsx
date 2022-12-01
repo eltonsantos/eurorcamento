4c1ff2e99b112b5e208f9335c7c82086cf100141
Elton Santos <eltonaxl@hotmail.com>
2022-12-01 03:38:37 -0300

    So many shits in my code, Jesus [2]

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,16 @@
-import { useState } from 'react';
 import Modal from 'react-modal';
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Login } from './components/Login';
-import { Dashboard } from './components/Dashboard';
-import { Header } from './components/Header';
-import { NewTransactionModal } from './components/NewTransactionModal';
+import Routes from './routes';
+import { BrowserRouter } from "react-router-dom"
 import { TransactionsProvider } from './hooks/useTransactions';
-
-import { GlobalStyle } from './styles/global';
-import { GlobalStyleLogin } from './styles/globalLogin';
-import { Footer } from './components/Footer';
-import { Menu } from './components/Menu';
 
 Modal.setAppElement('#root')
 
 export function App() {
-
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-  
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true)
-  }
-
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false)
-  }
   
   return (
     <TransactionsProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Login />
-              <GlobalStyleLogin />
-            </>
-          } />
-          <Route path="/transactions" element={
-            <>
-              <Menu />
-              <Header
-                onOpenNewTransactionModal={handleOpenNewTransactionModal}
-              />
-              <Dashboard />
-              <NewTransactionModal
-                isOpen={isNewTransactionModalOpen}
-                onRequestClose={handleCloseNewTransactionModal}
-              />
-              <Footer />
-              <GlobalStyle />
-            </>
-          } />
-        </Routes>
+        <Routes />   
       </BrowserRouter>
     </TransactionsProvider>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,18 @@ import { useState } from 'react';
 import App from './app.routes';
 import Auth from './auth.routes';
 
-import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+
 const Routes = () => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <Routes>
-      { logged ? <Auth /> : <App />}
-    </Routes>
+    <Route element={
+      <>
+        { isLoggedIn ? <Auth /> : <App />}
+      </>
+    } />
   )
 }
 

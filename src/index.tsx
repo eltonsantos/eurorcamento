d42f5a7b4639ce93d3000b0c50d173224594d65f
Elton Santos <eltonaxl@hotmail.com>
2022-12-10 04:33:56 -0300

    Commenting mirage s code

--- src/index.tsx
+++ src/index.tsx
@@ -1,65 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer, Model } from 'miragejs';
+// import { createServer, Model } from 'miragejs';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { TransactionsProvider } from './hooks/useTransactions';
 import { AuthProvider } from './hooks/useAuth';
 
-createServer({
+// createServer({
 
-  models: {
-    transaction: Model
-  },
+//   models: {
+//     transaction: Model
+//   },
 
-  seeds(server) {
-    server.db.loadData({
-      // transactions: [
-      //   {
-      //     id: 1,
-      //     title: 'Freelance de Website',
-      //     type: 'deposit',
-      //     category: 'Dev',
-      //     amount: 6000,
-      //     createdAt: new Date('2022-02-12 09:00:00'),
-      //   },
-      //   {
-      //     id: 2,
-      //     title: 'Aluguel',
-      //     type: 'withdraw',
-      //     category: 'Casa',
-      //     amount: 2000,
-      //     createdAt: new Date('2022-02-14 11:00:00'),
-      //   },
-      // ]
-    })
-  },
+//   seeds(server) {
+//     server.db.loadData({
+//       // transactions: [
+//       //   {
+//       //     id: 1,
+//       //     title: 'Freelance de Website',
+//       //     type: 'deposit',
+//       //     category: 'Dev',
+//       //     amount: 6000,
+//       //     createdAt: new Date('2022-02-12 09:00:00'),
+//       //   },
+//       //   {
+//       //     id: 2,
+//       //     title: 'Aluguel',
+//       //     type: 'withdraw',
+//       //     category: 'Casa',
+//       //     amount: 2000,
+//       //     createdAt: new Date('2022-02-14 11:00:00'),
+//       //   },
+//       // ]
+//     })
+//   },
 
-  routes() {
-    this.namespace = 'api';
+//   routes() {
+//     this.namespace = 'api';
     
-    this.get('/transactions', () => {
-      return this.schema.all('transaction')
-    });
+//     this.get('/transactions', () => {
+//       return this.schema.all('transaction')
+//     });
 
-    this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody);
-      return schema.create('transaction', data);
-    });
+//     this.post('/transactions', (schema, request) => {
+//       const data = JSON.parse(request.requestBody);
+//       return schema.create('transaction', data);
+//     });
 
-    this.put('/transactions/:id', (schema, request): any => {
-      const id = request.params.id
-      return schema.find('transaction', id)?.update;
-    })
+//     this.put('/transactions/:id', (schema, request): any => {
+//       const id = request.params.id
+//       return schema.find('transaction', id)?.update;
+//     })
 
-    this.del('/transactions/:id', (schema, request): any => { // Por que esse any??
-      const id = request.params.id // Vejo que ta sendo chamada, mas não acho que esteja fazendo nada
-      return schema.find('transaction', id)?.destroy() // Não entendi por que fui obrigado a deixar o opcional ? Como por outra forma?
-    })
-
-    this.passthrough('https://identitytoolkit.googleapis.com/**')
-  }
-})
+//     this.del('/transactions/:id', (schema, request): any => { // Por que esse any??
+//       const id = request.params.id // Vejo que ta sendo chamada, mas não acho que esteja fazendo nada
+//       return schema.find('transaction', id)?.destroy() // Não entendi por que fui obrigado a deixar o opcional ? Como por outra forma?
+//     })
+       // To firebase
+//     this.passthrough('https://identitytoolkit.googleapis.com/**') 
+//   }
+// })
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
